f91ee4b9f5cc66f1842124d8eac66d0e900d9599
arsha4257 <138533010+arsha4257@users.noreply.github.com>
2023-11-13 16:48:13 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-OT2bBCKXcsSTeJUKPlKe8RBtwrjlXx8",
+    authDomain: "letscahtwedapp.firebaseapp.com",
+    databaseURL: "https://letscahtwedapp-default-rtdb.firebaseio.com",
+    projectId: "letscahtwedapp",
+    storageBucket: "letscahtwedapp.appspot.com",
+    messagingSenderId: "787020137207",
+    appId: "1:787020137207:web:104dc8c0c68481f29789f2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
